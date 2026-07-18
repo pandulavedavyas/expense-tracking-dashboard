8c7789c20b38098721f5c6741023b7aec151f082
pandulavedavyas <pandulavedavyas@gmail.com>
2026-07-18 11:13:52 +0530

    Updated config and requirements

--- config.py
+++ config.py
@@ -3,12 +3,9 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'expense-tracker-secret-key-change-in-production')
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL',
-        'sqlite:///{}'.format(
-            os.path.join('/tmp', 'database.db')
-            if os.environ.get('VERCEL')
-            else os.path.join(os.path.dirname(os.path.abspath(__file__)), 'instance', 'database.db')
-        )
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///{}'.format(
+        os.path.join('/tmp', 'database.db')
+        if os.environ.get('VERCEL')
+        else os.path.join(os.path.dirname(os.path.abspath(__file__)), 'instance', 'database.db')
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
